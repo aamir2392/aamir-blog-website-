cfe3e8e63b0f76d0b76247c1ae92a6973c6e8250
aamir <aamir@gmail.com>
2024-06-18 15:53:38 +0500

    first commit

---
+++ src/Create.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Aamir Habib");
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author };
+        console.log(blog);
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": 'application/json' },
+            body: JSON.stringify(blog)
+        }
+        ).then(() => {
+            console.log('New Blog Added');
+            setIsPending(false);
+            // history.go(-1);
+            history.push('/');
+        }
+        )
+    }
+
+    return ( 
+        <div className="create">
+            <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title: </label>
+                <input type="text" 
+                 required  
+                 value={title} 
+                 onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body: </label>
+                <textarea
+                 required
+                 onChange={(e) => setBody(e.target.value)}
+
+                ></textarea>
+                <label>Blog author: </label>
+                <select 
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Aamir Habib">Aamir Habib</option>
+                    <option value="Umair Habib">Umair Habib</option>
+                </select>
+                {!isPending && <button>Add Blog</button> }
+                {isPending && <button disabled>Adding Blog...</button> }
+                
+            </form>
+        </div>
+     );
+}
+ 
+export default Create;
